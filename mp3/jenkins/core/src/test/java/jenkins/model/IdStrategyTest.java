8362c00caaf56b7dd41d9fc124c010263a7e3d99
Michael <mschardein3679@gmail.com>
2020-10-30 18:03:13 -0400

    test file for q4

---
+++ mp3/jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
@@ -0,0 +1,19 @@
+package jenkins.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdStrategyTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(2, 2);
+	}
+
+}
